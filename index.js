de38bd7c5c5331c8b99e083371bd2c96e5b6b857
jonathan <cliffjonathan66@gmail.com>
2025-03-04 16:47:18 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+// 1. Declare companyName as a constant with value "Scuber"
+const companyName = "Scuber"; // Must use const
+
+// 2. Declare mostProfitableNeighborhood as a let variable and assign a value
+let mostProfitableNeighborhood = "Chelsea"; // Must use let
+
+// 3. Declare companyCeo as a let variable and assign a value
+let companyCeo = "Susan Smith"; // Must use let
